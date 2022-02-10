a57b43f41020a5fcb8616c2c2769271b0b50cb6c
decitre <emmanuel.decitre@here.com>
2022-02-11 00:26:35 +0100

    add several widgets designs

--- search/widgets/__init__.py
+++ search/widgets/__init__.py
@@ -1,130 +1,21 @@
-from IPython.display import display as Idisplay, JSON as IJSON, clear_output
+from IPython.display import display as Idisplay, JSON as IJSON, Markdown, clear_output, display_markdown
 from ipywidgets import HBox, VBox, Text, Button, Output, Layout
-from ipywidgets.widgets.widget import CallbackDispatcher
+from ipywidgets.widgets.widget import CallbackDispatcher, Widget
 from traitlets import observe
-from requests import Session
 
 from here_map_widget import GeoJSON, WidgetControl
 from here_map_widget import Platform, MapTile, TileLayer, Map
 from here_map_widget import ServiceNames, MapTileUrl
 
-from ..core import OneBoxBase
+from search.core import OneBoxBase
 
 from typing import Callable, Tuple, List, Awaitable
 from functools import reduce
 import asyncio
 
 
-class SearchFeatureCollection(GeoJSON):
-    bbox: Tuple[float, float, float, float]
-
-    def __init__(self, results: dict):
-        collection = {"type": "FeatureCollection", "features": []}
-        latitudes, longitudes = [], []
-        for item in results["items"]:
-            if "position" not in item:
-                continue
-            longitude, latitude = item["position"]["lng"], item["position"]["lat"]
-            latitudes.append(latitude)
-            longitudes.append(longitude)
-            categories = [c["name"][:10] for c in item["categories"]
-                          if c.get("primary")][0] if "categories" in item else None
-            collection["features"].append({"type": "Feature",
-                                           "geometry": {
-                                               "type": "Point",
-                                               "coordinates": [longitude, latitude]},
-                                           "properties": {"title": item["title"][:10],
-                                                          "categories": categories}})
-        if latitudes:
-            south, west, north, east = min(latitudes), min(longitudes), max(latitudes), max(longitudes)
-            collection["bbox"] = [south, west, north, east]
-            height = north-south
-            width = east-west
-            self.bbox = (south-height/6, north+height/6, east+width/6, west-width/6)
-            # [33.65, 49.46, 175.41, -109.68]
-        else:
-            self.bbox = None
-
-        GeoJSON.__init__(self, data=collection,
-                         show_bubble=True,
-                         point_style={
-                             "strokeColor": "white",
-                             "lineWidth": 1,
-                             "fillColor": "blue",
-                             "fillOpacity": 0.7,
-                             "radius": 5}
-                         )
-
-
-class TermsButtons(HBox):
-    default_layout = {'width': '280px'}
-    default_buttons_count = 3
-
-    def __init__(self, buttons_count: int=None, layout: dict=None):
-        if not isinstance(buttons_count, int):
-            buttons_count = TermsButtons.default_buttons_count
-        width = int(100/buttons_count)
-        box_layout = Layout(display="flex", justify_content="center", width=f"{width}%", border="solid 1px")
-        buttons = []
-        for i in range(buttons_count):
-            button = Button(layout=box_layout)
-            buttons.append(button)
-        HBox.__init__(self, buttons, layout=layout or TermsButtons.default_layout)
-
-    def on_click(self, handler):
-        for button in self.children:
-            button.on_click(handler)
-
-
-class SearchTermsBox(VBox):
-    def __init__(self, wquery: "OneBoxMap", query_terms: TermsButtons):
-        VBox.__init__(self, [wquery, query_terms])
-
-
-class PositionMap(Map):
-    default_zoom_level = 12
-    default_layout = {'height': '600px'}
-
-    def __init__(self, api_key: str,
-                 center: List[float],
-                 position_handler: Callable[[float, float], None]=None,
-                 **kvargs):
-
-        platform = Platform(api_key=api_key, services_config={
-            ServiceNames.maptile: {
-                MapTileUrl.scheme: "https",
-                MapTileUrl.host: "maps.ls.hereapi.com",
-                MapTileUrl.path: "maptile/2.1",
-            }
-        })
-        map_tile = MapTile(
-            tile_type="maptile",
-            scheme="normal.day",
-            tile_size=256,
-            format="png",
-            platform=platform
-        )
-        maptile_layer = TileLayer(provider=map_tile, style={"max": 22})
-        Map.__init__(self,
-                     api_key=api_key,
-                     center=center,
-                     zoom=kvargs.pop('zoom', PositionMap.default_zoom_level),
-                     basemap=maptile_layer,
-                     layout = kvargs.pop('layout', PositionMap.default_layout))
-        if position_handler:
-            self.set_position_handler(position_handler)
-
-    def set_position_handler(self, position_handler: Callable[[float, float], None]):
-        def observe(change):
-            if change.type == "change": # TODO: test if this test is necessary
-                if change.name in "center":
-                    position_handler(*change.new[:2])
-                elif change.name == "zoom":
-                    position_handler(*self.center)
-        self.observe(observe)
-
-
 class SubmittableText(Text):
+    """A ipywidgets Text class enhanced with a on_submit() method"""
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -156,10 +47,13 @@ class SubmittableText(Text):
 
 
 class SubmittableTextBox(HBox):
+    """A ipywidgets HBox made of a SubmittableText and a lens Button"""
     default_icon = 'fa-search'
+    default_button_width = '32px'
 
     def __init__(self, *args, **kwargs):
-        self.lens = Button(icon=kwargs.pop('icon', SubmittableTextBox.default_icon), layout={'width': '32px'})
+        self.lens = Button(icon=kwargs.pop('icon', SubmittableTextBox.default_icon),
+                           layout={'width': SubmittableTextBox.default_button_width})
         self.text = SubmittableText(*args, layout=kwargs.pop('layout', Layout()), **kwargs)
         super().__init__([self.text, self.lens], **kwargs)
 
@@ -205,13 +99,168 @@ class SubmittableTextBox(HBox):
         return future
 
 
+class SearchFeatureCollection(GeoJSON):
+    bbox: Tuple[float, float, float, float]
+
+    def __init__(self, results: dict):
+        collection = {"type": "FeatureCollection", "features": []}
+        latitudes, longitudes = [], []
+        for item in results["items"]:
+            if "position" not in item:
+                continue
+            longitude, latitude = item["position"]["lng"], item["position"]["lat"]
+            latitudes.append(latitude)
+            longitudes.append(longitude)
+            categories = [c["name"] for c in item["categories"]
+                          if c.get("primary")][0] if "categories" in item else None
+            collection["features"].append({"type": "Feature",
+                                           "geometry": {
+                                               "type": "Point",
+                                               "coordinates": [longitude, latitude]},
+                                           "properties": {"title": item["title"],
+                                                          "categories": categories}})
+        if latitudes:
+            south, west, north, east = min(latitudes), min(longitudes), max(latitudes), max(longitudes)
+            collection["bbox"] = [south, west, north, east]
+            height = north-south
+            width = east-west
+            self.bbox = (south-height/6, north+height/6, east+width/6, west-width/6)
+            # [33.65, 49.46, 175.41, -109.68]
+        else:
+            self.bbox = None
+
+        GeoJSON.__init__(self, data=collection,
+                         show_bubble=True,
+                         point_style={
+                             "strokeColor": "white",
+                             "lineWidth": 1,
+                             "fillColor": "blue",
+                             "fillOpacity": 0.7,
+                             "radius": 5}
+                         )
+
+
+class TermsButtons(HBox):
+    """A HBox containing a list of Buttons"""
+    default_layout = {'width': '280px'}
+    default_buttons_count = 3
+
+    def __init__(self, buttons_count: int=None, layout: dict=None):
+        if not isinstance(buttons_count, int):
+            buttons_count = TermsButtons.default_buttons_count
+        width = int(100/buttons_count)
+        box_layout = Layout(display="flex", justify_content="center", width=f"{width}%", border="solid 1px")
+        buttons = []
+        for i in range(buttons_count):
+            button = Button(layout=box_layout)
+            buttons.append(button)
+        HBox.__init__(self, buttons, layout=layout or TermsButtons.default_layout)
+
+    def on_click(self, handler):
+        for button in self.children:
+            button.on_click(handler)
+
+    def set(self, values: list[str]):
+        for i, button in enumerate(self.children):
+            try:
+                button.description = values[i]
+            except IndexError:
+                button.description = ' '
+
+
+class SearchTermsBox(VBox):
+    def __init__(self, text: "SubmittableTextBox", terms_buttons: TermsButtons):
+        self.text = text
+        self.terms_buttons = terms_buttons
+        VBox.__init__(self, [text, terms_buttons])
+
+
+class PositionMap(Map):
+    default_zoom_level = 12
+    default_layout = {'height': '700px'}
+
+    def __init__(self, api_key: str,
+                 center: List[float],
+                 position_handler: Callable[[float, float], None]=None,
+                 **kvargs):
+
+        platform = Platform(api_key=api_key, services_config={
+            ServiceNames.maptile: {
+                MapTileUrl.scheme: "https",
+                MapTileUrl.host: "maps.ls.hereapi.com",
+                MapTileUrl.path: "maptile/2.1",
+            }
+        })
+        map_tile = MapTile(
+            tile_type="maptile",
+            scheme="normal.day",
+            tile_size=256,
+            format="png",
+            platform=platform
+        )
+        maptile_layer = TileLayer(provider=map_tile, style={"max": 22})
+        Map.__init__(self,
+                     api_key=api_key,
+                     center=center,
+                     zoom=kvargs.pop('zoom', PositionMap.default_zoom_level),
+                     basemap=maptile_layer,
+                     layout = kvargs.pop('layout', PositionMap.default_layout))
+        if position_handler:
+            self.set_position_handler(position_handler)
+
+    def set_position_handler(self, position_handler: Callable[[float, float], None]):
+        def observe(change):
+            if change.type == "change": # TODO: test if this test is necessary
+                if change.name in "center":
+                    position_handler(*change.new[:2])
+                elif change.name == "zoom":
+                    position_handler(*self.center)
+        self.observe(observe)
+
+
+class Design(Widget):
+    @classmethod
+    def one(cls,
+            query_text: SubmittableTextBox,
+            map: PositionMap,
+            terms: TermsButtons,
+            out: HBox):
+        search_box = VBox([query_text, terms])
+        widget_control = WidgetControl(widget=search_box, alignment="TOP_LEFT", name="search", transparent_bg=True)
+        map.add_control(widget_control)
+        map.zoom_control_instance.alignment = "RIGHT_TOP"
+        return HBox([map, out])
+
+    @classmethod
+    def two(self,
+            query_text: SubmittableTextBox,
+            map: PositionMap,
+            terms: TermsButtons,
+            out: HBox):
+        search_box = VBox([query_text, terms, out])
+        return HBox([search_box, map])
+
+    @classmethod
+    def three(cls,
+            query_text: SubmittableTextBox,
+            map: PositionMap,
+            terms: TermsButtons,
+            out: HBox):
+        search_box = VBox([query_text, terms, out])
+        widget_control = WidgetControl(widget=search_box, alignment="TOP_LEFT", name="search", transparent_bg=True)
+        map.add_control(widget_control)
+        map.zoom_control_instance.alignment = "RIGHT_TOP"
+        return map
+
+
 class OneBoxMap(SubmittableTextBox, OneBoxBase):
-    default_results_limit = 20
+    default_results_limit = 10
     default_suggestions_limit = 5
     default_terms_limit = 3
     minimum_zoom_level = 11
-    default_layout = {'width': '240px'}
+    default_search_box_layout = {'width': '240px'}
     default_placeholder = "free text"
+    default_output_format = 'text'
 
     def __init__(self,
                  language: str,
@@ -220,11 +269,13 @@ class OneBoxMap(SubmittableTextBox, OneBoxBase):
                  results_limit: int=None,
                  suggestions_limit: int=None,
                  terms_limit: int=None,
+                 design: Callable=None,
+                 output_format: str=None,
                  autosuggest_automatic_recenter: bool=False,
                  **kwargs):
 
         SubmittableTextBox.__init__(self,
-                                    layout=kwargs.pop('layout', OneBoxMap.default_layout),
+                                    layout=kwargs.pop('layout', OneBoxMap.default_search_box_layout),
                                     placeholder=kwargs.pop('placeholder', OneBoxMap.default_placeholder),
                                     **kwargs)
 
@@ -236,43 +287,28 @@ class OneBoxMap(SubmittableTextBox, OneBoxBase):
                             terms_limit=terms_limit or OneBoxMap.default_terms_limit)
 
         self.layer = None
-        self.as_url = f'https://autosuggest.search.hereapi.com/v1/autosuggest?apiKey={self.api_key}'
-        self.ds_url = f'https://discover.search.hereapi.com/v1/discover?apiKey={self.api_key}'
-        self.session = Session()
-
-        self.init_terms_buttons()
+        self.query_terms = TermsButtons(OneBoxMap.default_terms_limit)
+        self.query_terms.on_click(self.__get_terms_buttons_handler())
         self.map = PositionMap(api_key=self.api_key, center=[latitude, longitude])
-        self.add_search_control_to_map()
-        self.output_widget = Output(layout={'width': '450px'})
+        self.out = HBox([Output(layout={'width': '450px'})])
+        self.design = (design or Design.two)(self, self.map, self.query_terms, self.out)
+        self.output_format = output_format or OneBoxMap.default_output_format
 
-    def run(self):
-        Idisplay(HBox([self.map, self.output_widget]))
-        OneBoxBase.run(self)
+    def __get_terms_buttons_handler(self):
+        def on_terms_click_handler(button):
+            # replace the last token with the clicked button description and a whitespace
+            tokens = self.text.value.strip().split(' ')
+            if tokens:
+                head = tokens[:-1]
+                head.extend([button.description.strip(), ''])
+                self.text.value = ' '.join(head)
+        return on_terms_click_handler
 
     def get_search_center(self):
         return self.map.center
 
-    def init_terms_buttons(self):
-        self.query_terms = TermsButtons(OneBoxMap.default_terms_limit)
-        def on_terms_click_handler(change):
-            tokens = self.text.value.strip().split(' ')
-            if tokens:
-                head, tail = tokens[:-1], tokens[-1]
-                head.extend([change.description.strip(), ''])
-                self.text.value = ' '.join(head)
-        self.query_terms.on_click(on_terms_click_handler)
-
-    def add_search_control_to_map(self):
-        self.search_box = SearchTermsBox(self, self.query_terms)
-        widget_control = WidgetControl(widget=self.search_box, alignment="TOP_LEFT", name="search")
-        self.map.add_control(widget_control)
-        self.map.zoom_control_instance.alignment = "RIGHT_TOP"
-
     def display_terms(self, autosuggest_resp):
-        for i in range(OneBoxMap.default_terms_limit):
-            self.query_terms.children[i].description = ' '
-        for i, term in enumerate(autosuggest_resp.get('queryTerms', [])):
-            self.query_terms.children[i].description = term['term']
+        self.query_terms.set([term['term'] for term in autosuggest_resp.get('queryTerms', [])])
 
     def display_suggestions(self, autosuggest_resp):
         self.display_result_list(autosuggest_resp)
@@ -294,8 +330,7 @@ class OneBoxMap(SubmittableTextBox, OneBoxBase):
 
     def clear_query_text(self):
         self.text.value = ''
-        for i in range(OneBoxMap.default_terms_limit):
-            self.query_terms.children[i].description = ' '
+        self.query_terms.set([])
 
     def display_result_map(self, resp, update_search_center=False):
         if self.layer:
@@ -307,11 +342,52 @@ class OneBoxMap(SubmittableTextBox, OneBoxBase):
             if len(resp["items"]) == 1:
                 self.map.zoom = OneBoxMap.minimum_zoom_level
 
+    @staticmethod
+    def get_primary_category(place_item):
+        primary_category = [c for c in place_item.get("categories", []) if c.get("primary")][0]
+        category_id = primary_category["id"]
+        category_name = primary_category["name"]
+        return category_id, category_name
+
+    @staticmethod
+    def get_www(place_item, category_id) -> str:
+        for contact in place_item["contacts"]:
+            for www in contact.get("www", []):
+                for category in www.get("categories", []):
+                    if category_id == category["id"]:
+                        return www["value"]
+        else:
+            for contact in place_item["contacts"]:
+                for www in contact.get("www", []):
+                    return www["value"]
+            else:
+                return None
+
     def display_result_list(self, resp):
-        if True:
-            self.output_widget.clear_output(wait=True)
-            self.output_widget.append_display_data(IJSON(resp))
-        else: # does not work for key strokes... TODO: Check why
-            with self.output_widget:
-                clear_output(wait=True)
-                Idisplay(IJSON(resp))
+        # https://stackoverflow.com/questions/66704546/why-cannot-i-print-in-jupyter-lab-using-ipywidgets-class
+        old_out = self.out.children[0]
+        out = Output(layout={'width': '450px'})
+        if self.output_format == 'json':
+            out.append_display_data(IJSON(data=resp, expanded=False, root='response'))
+        else:
+            text = ['| | |', '|-|-|']
+            for i, item in enumerate(resp["items"]):
+                if "contacts" in item:
+                    category_id, category_name = OneBoxMap.get_primary_category(item)
+                    www = OneBoxMap.get_www(item, category_id)
+                    title = f"**[{item['title']}]({www})**" if www else f"**{item['title']}**"
+                else:
+                    title = f"**{item['title']}**"
+                text.append(f"| {i: <2} | {title} |")
+                if item["resultType"] in ("categoryQuery", "chainQuery"):
+                    text.append(f"| | <sup>{item['resultType']}</sup> |")
+                elif item["resultType"] == 'place':
+                    address = item['address']['label'].partition(', ')[-1]
+                    text.append(f"| | <sup>{address}</sup> |")
+            out.append_display_data(Markdown("\n".join(text)))
+        self.out.children = [out]
+        old_out.close()
+
+    def run(self):
+        Idisplay(self.design)
+        OneBoxBase.run(self)
