857dc90a385fab977c5cb3b0069a53a56ad9d687
Emmanuel Decitre <590094+decitre@users.noreply.github.com>
2023-08-27 17:14:19 +0200

    Update to ipywidgets 8.1.0 (#9)
    * readme cosmetics
    * Update to newer python packages
    * bump version 0.8.6 -> 0.9.0
    
    ---------
    
    Signed-off-by: Emmanuel Decitre <emmanuel.decitre@here.com>

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 [![Python package](https://github.com/heremaps/here-search-demo/actions/workflows/test.yml/badge.svg)](https://github.com/heremaps/here-search-demo/actions/workflows/test.yml)
 [![codecov](https://codecov.io/gh/heremaps/here-search-demo/branch/main/graph/badge.svg?token=MVFCS4BUFN)](https://codecov.io/gh/heremaps/here-search-demo)
+[![lite-badge](https://jupyterlite.rtfd.io/en/latest/_static/badge.svg)](https://decitre.github.io/here-search-demo-updated/lab/?path=demo.ipynb)
+
 
 # HERE Search notebooks
 
@@ -16,7 +18,7 @@ The notebooks can be [used in your browser][3] without further installation.
 If you need to install the notebooks or the underlying library on your workstation, run preferably in a virtual environment:
 
    ```
-   pip install here-search-demo
+   pip install 'here-search-demo[lab]'
    ```
 
 Link the virtual environment to a IPython kernel:
@@ -44,6 +46,6 @@ This project is licensed under the MIT license - see the [LICENSE](./LICENSE) fi
 
 [1]: https://developer.here.com/documentation/geocoding-search-api/dev_guide/topics/quick-start.html#get-an-api-key
 [2]: https://github.com/heremaps/here-search-demo/blob/main/docs/developers.md
-[3]: https://heremaps.github.io/here-search-demo/lab/?path=demo.ipynb
+[3]: https://decitre.github.io/here-search-demo-updated/lab/?path=demo.ipynb
 [4]: https://developer.here.com/documentation/geocoding-search-api/dev_guide/index.html
 [5]: https://www.here.com/get-started/pricing
--- src/here_search/demo/scripts/lite-run.sh
+++ src/here_search/demo/scripts/lite-run.sh
@@ -14,6 +14,9 @@ set -e
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 ROOT_DIR="$SCRIPT_DIR"/../../../..
 NOTEBOOKS_DIR="$SCRIPT_DIR"/../notebooks
+RED='\033[0;31m'
+YELLOW='\033[33m'
+NC='\033[0m'
 
 serve=1
 while getopts n option
@@ -24,20 +27,33 @@ do
     esac
 done
 
+printf "${YELLOW}Build workspace${NC}\n"
 rm -rf workspace
 mkdir -p workspace/content
-(cd workspace
- python -m venv venv
- source venv/bin/activate
- python -m pip -q install --upgrade pip
- pip install -r "$ROOT_DIR"/requirements/util.txt
- pip install -r "$ROOT_DIR"/requirements/lite_run.txt
- #pip wheel -e .. --src .. --wheel-dir content --no-deps --no-binary ":all:"
- python -m build "$ROOT_DIR" --wheel --outdir content --skip-dependency-check
- wheel unpack "$(find content -name '*.whl')" -d package
- sed -i.bak '/Requires/d' "$(find package -name METADATA)"
- wheel pack "$(dirname "$(find package -name "*.dist-info")")" -d content
- cp "$NOTEBOOKS_DIR"/*.ipynb content/
- jupyter lite build --contents content --output-dir public --lite-dir public
- [ $serve -eq 1 ] && jupyter lite serve --lite-dir public
-)
+
+printf "${YELLOW}Create venv${NC}\n"
+pushd workspace
+python -m venv venv
+source venv/bin/activate
+python -m pip -q install --upgrade pip
+
+printf "${YELLOW}Install packages${NC}\n"
+pip install -r "$ROOT_DIR"/requirements/build.txt
+pip install wheel build
+
+printf "${YELLOW}Build here-search-demo wheel${NC}\n"
+#pip wheel -e .. --src .. --wheel-dir content --no-deps --no-binary ":all:"
+python -m build "$ROOT_DIR" --wheel --outdir content --skip-dependency-check
+wheel unpack "$(find content -name '*.whl')" -d package
+sed -i.bak '/Requires/d' "$(find package -name METADATA)"
+wheel pack "$(dirname "$(find package -name "*.dist-info")")" -d content
+
+printf "${YELLOW}Build Jupyter Lite static page${NC}\n"
+cp "$NOTEBOOKS_DIR"/*.ipynb content/
+jupyter lite build --contents content --output-dir public --lite-dir public
+
+if [ $serve -eq 1 ]; then
+    printf "${YELLOW}Serve....${NC}\n"
+    jupyter lite serve --lite-dir public
+fi
+popd
